c4ac2aff184c1720b69b14d0a3ec0330d82def1c
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2023-10-20 11:46:33 +0200

    refactor: add an auto-increment id field, so 'persistence' works

--- excel-importer/src/com/axonivy/util/excel/importer/EntityClassReader.java
+++ excel-importer/src/com/axonivy/util/excel/importer/EntityClassReader.java
@@ -7,13 +7,15 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
 import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.scripting.dataclass.DataClassFieldModifier;
 import ch.ivyteam.ivy.scripting.dataclass.IDataClassManager;
 import ch.ivyteam.ivy.scripting.dataclass.IEntityClass;
+import ch.ivyteam.ivy.scripting.dataclass.IEntityClassField;
 import ch.ivyteam.ivy.scripting.dataclass.IProjectDataClassManager;
 
 public class EntityClassReader {
 
-  private final IProjectDataClassManager manager;
+  public final IProjectDataClassManager manager;
 
   @SuppressWarnings("restriction")
   public EntityClassReader() {
@@ -27,20 +29,31 @@ public class EntityClassReader {
   public IEntityClass getEntity(Path filePath) {
     Workbook wb = ExcelLoader.load(filePath);
     String dataName = StringUtils.substringBeforeLast(filePath.getFileName().toString(), ".");
+    dataName = StringUtils.capitalize(dataName);
     return toEntity(wb.getSheetAt(0), dataName);
   }
 
-  private IEntityClass toEntity(Sheet sheet, String dataName) {
+  public IEntityClass toEntity(Sheet sheet, String dataName) {
     String fqName = manager.getDefaultNamespace()+"."+dataName;
     if (manager.findDataClass(fqName) != null) {
       throw new RuntimeException("entity "+fqName+" already exists");
     }
     var entity = manager.createEntityClass(fqName, null);
 
+    withIdField(entity);
     ExcelReader.parseColumns(sheet).stream().forEachOrdered(col -> {
       entity.addField(fieldName(col.name()), col.type().getName());
     });
     return entity;
+  }
+
+  private void withIdField(IEntityClass entity) {
+    IEntityClassField id = entity.addField("id", Integer.class.getSimpleName());
+    id.setDatabaseFieldName("id");
+    id.addModifier(DataClassFieldModifier.PERSISTENT);
+    id.addModifier(DataClassFieldModifier.ID);
+    id.addModifier(DataClassFieldModifier.GENERATED);
+    id.setComment("Identifier");
   }
 
   private String fieldName(String colName) {
